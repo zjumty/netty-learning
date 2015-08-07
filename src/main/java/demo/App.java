6542b6c0b96fcd4049905b3c3ddb86c604afca48
Terry Ma <zjumty@aliyun.com>
2015-08-07 19:13:45 +0800

    初始化

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: 这里要写注释的!
+ */
+public class App {
+    public static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOG.info("App...");
+    }
+}
